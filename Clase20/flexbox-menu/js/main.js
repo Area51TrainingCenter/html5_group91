9466254203ab51d1120e7cb82889bf36bae77e20
alumno <alumno@Mac-mini-de-Mac.local>
2017-07-06 21:47:45 -0500

    Clase20

---
+++ Clase20/flexbox-menu/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+/*	$("header a").on("click", function(e){
+		e.preventDefault()
+		$("article.menu").toggleClass("mostrar")
+	})*/
+
+	new WOW().init();
+
+	smoothScroll.init();
+
+})
